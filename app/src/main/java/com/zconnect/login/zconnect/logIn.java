debe294fe1d9b738305135d9ee6cbd655fb8af8d
G-LOKi <lokesh96@gamil.com>
2016-08-18 19:14:14 +0530

    Initial commit

---
+++ app/src/main/java/com/zconnect/login/zconnect/logIn.java
@@ -0,0 +1,20 @@
+package com.zconnect.login.zconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class logIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+
+
+    private void TestUpload(){
+        int i =1+2;
+
+    }
+}
+
