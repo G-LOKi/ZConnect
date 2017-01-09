3eaa6970bb43719d8ca35c9f70e307b7563012c4
G-LOKi <lokesh96@gamil.com>
2017-01-09 23:46:35 +0530

    Event Improved

--- app/src/main/java/com/zconnect/login/zconnect/AllEvents.java
+++ app/src/main/java/com/zconnect/login/zconnect/AllEvents.java
@@ -130,7 +130,7 @@ public class AllEvents extends AppCompatActivity {
             Reminder.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    
+
                 }
             });
         }
