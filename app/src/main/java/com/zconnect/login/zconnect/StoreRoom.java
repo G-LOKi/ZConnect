e270a4f0e02787faeeb646c50645e96d4b061936
G-LOKi <lokesh96@gamil.com>
2017-01-13 00:22:38 +0530

    StoreRoom middle of adding reserve 5

--- app/src/main/java/com/zconnect/login/zconnect/StoreRoom.java
+++ app/src/main/java/com/zconnect/login/zconnect/StoreRoom.java
@@ -10,11 +10,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
@@ -26,6 +28,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -43,7 +46,6 @@ public class StoreRoom extends AppCompatActivity {
         setContentView(R.layout.activity_store_room);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         mProductList = (RecyclerView) findViewById(R.id.productList);
             mProductList.setHasFixedSize(true);
             mProductList.setLayoutManager(new LinearLayoutManager(this));
@@ -73,12 +75,15 @@ public class StoreRoom extends AppCompatActivity {
                 ProductViewHolder.class,
                 mDatabase
         ) {
+
             @Override
             protected void populateViewHolder(ProductViewHolder viewHolder, Product model, int position) {
+                viewHolder.defaultSwitch(model.getKey());
                 viewHolder.setSwitch(model.getKey());
                 viewHolder.setProductName(model.getProductName());
                 viewHolder.setProductDesc(model.getProductDescription());
                 viewHolder.setImage(getApplicationContext(), model.getImage());
+
             }
         };
         mProductList.setAdapter(firebaseRecyclerAdapter);
@@ -97,79 +102,136 @@ public class StoreRoom extends AppCompatActivity {
         public ProductViewHolder(View itemView) {
             super(itemView);
             mView = itemView;
+
+        }
+
+        public void defaultSwitch(final String key)
+        {
+            ReserveStatus = (TextView) mView.findViewById(R.id.switch1);
+            mReserve = (Switch) mView.findViewById(R.id.switch1);
+            ReserveReference = FirebaseDatabase.getInstance().getReference().child("ZConnect/Users");
+            mAuth = FirebaseAuth.getInstance();
+            FirebaseUser user = mAuth.getCurrentUser();
+            final String userId = user.getUid();
+//            Toast.makeText(mView.getContext(), userId, Toast.LENGTH_SHORT).show();
+
+            ReserveReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    ReservedUid = (String)dataSnapshot.child(userId +"/Reserved").getValue();
+//                    Toast.makeText(mView.getContext(), ReservedUid, Toast.LENGTH_SHORT).show();
+
+//                    if(ReservedUid == null)
+//                        ReservedUid = "   ";
+
+                    keyList = ReservedUid.split(" ");
+
+                    Toast.makeText(mView.getContext(), ReservedUid, Toast.LENGTH_SHORT).show();
+                    //check the current state before we display the screen
+                    List<String> list = new ArrayList<String>(Arrays.asList(keyList));
+                    if (list.contains(key))
+                    {
+                        mReserve.setChecked(true);
+                        Toast.makeText(mView.getContext(), "Contains Id", Toast.LENGTH_SHORT).show();
+
+                    }
+                    else {
+                        mReserve.setChecked(false);
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
         }
 
         public void setSwitch(final String key)
         {
-
             ReserveStatus = (TextView) mView.findViewById(R.id.switch1);
             mReserve = (Switch) mView.findViewById(R.id.switch1);
             //set the switch to ON
-            mReserve.setChecked(false);
+
             //attach a listener to check for changes in state
+            ReserveReference = FirebaseDatabase.getInstance().getReference().child("ZConnect/Users");
+            mAuth = FirebaseAuth.getInstance();
+            FirebaseUser user = mAuth.getCurrentUser();
+            final String userId = user.getUid();
+            Toast.makeText(mView.getContext(), userId, Toast.LENGTH_SHORT).show();
+
+            ReserveReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    ReservedUid = (String)dataSnapshot.child(userId +"/Reserved").getValue();
+                    Toast.makeText(mView.getContext(), ReservedUid, Toast.LENGTH_SHORT).show();
+
+                    if(ReservedUid == null)
+                        ReservedUid = "   ";
+
+                    keyList = ReservedUid.split(" ");
+
+
+
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+
+
 
             mReserve.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
 
                 @Override
                 public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
 
-                    ReserveReference = FirebaseDatabase.getInstance().getReference().child("ZConnect/Users");
-                    mAuth = FirebaseAuth.getInstance();
-                    FirebaseUser user = mAuth.getCurrentUser();
-                    final String userId = user.getUid();
-
-//                    ReserveReference.addValueEventListener(new ValueEventListener() {
-//                        @Override
-//                        public void onDataChange(DataSnapshot dataSnapshot) {
-//                            ReservedUid = (String)dataSnapshot.child(userId +"/Reserved").getValue();
-//
-////                            if(ReservedUid == null)
-////                                ReservedUid = "";
-//                            keyList = ReservedUid.split("\\s");
-//                        }
-//
-//                        @Override
-//                        public void onCancelled(DatabaseError databaseError) {
-//
-//                        }
-//                    });
 
 
                     if(isChecked){
                         ReserveStatus.setText("Product Reserved");
-//                        List list = (List) Arrays.asList(keyList);
-//                        list.add(key);
-//                        ReservedUid = TextUtils.join(" ", list);
-//                        DatabaseReference newPost = ReserveReference.child(userId);
-//                        Map<String, Object> childUpdates = new HashMap<>();
-//                        childUpdates.put("Reserved", ReservedUid);
-//                        newPost.updateChildren(childUpdates);
+                        Toast.makeText(mView.getContext(), key, Toast.LENGTH_SHORT).show();
+                        List<String> list = new ArrayList<String>(Arrays.asList(keyList));
+                        if (!list.contains(key))
+                        list.add(key);
+
+                        ReservedUid = TextUtils.join(" ", list);
+
+                        Toast.makeText(mView.getContext(),ReservedUid, Toast.LENGTH_SHORT).show();
+                        DatabaseReference newPost = ReserveReference.child(userId);
+                        Map<String, Object> childUpdates = new HashMap<>();
+                        childUpdates.put("Reserved", ReservedUid);
+                        newPost.updateChildren(childUpdates);
 
 
 
                     } else{
                         ReserveStatus.setText("Reserve Now");
-//                        List list = (List) Arrays.asList(keyList);
+                           List<String> list = new ArrayList<String>(Arrays.asList(keyList));
 //                        //remove
-//                        list.remove(userId);
-//                        ReservedUid = TextUtils.join(", ", list);
-//                        DatabaseReference newPost = ReserveReference.child(userId);
-//                        Map<String, Object> childUpdates = new HashMap<>();
-//                        childUpdates.put("Reserved", ReservedUid);
-//                        newPost.updateChildren(childUpdates);
+                        list.remove(key);
+
+                        ReservedUid = TextUtils.join(" ", list);
+                        DatabaseReference newPost = ReserveReference.child(userId);
+                        Map<String, Object> childUpdates = new HashMap<>();
+                        childUpdates.put("Reserved", ReservedUid);
+                        newPost.updateChildren(childUpdates);
                     }
+
+
                 }
             });
 
-            //check the current state before we display the screen
-            if(mReserve.isChecked()){
-                ReserveStatus.setText("Switch is currently ON");
-            }
-            else {
-                ReserveStatus.setText("Switch is currently OFF");
-            }
+
+
 
         };
+
 
         public void setProductName(String productName){
 
